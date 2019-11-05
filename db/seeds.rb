c66f918dc47d578410b8fe1c906153307bcff8be
github username <github email address>
2019-11-05 14:33:25 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # User.create(name: "Nadia", username: "nadiaf", password: "password")
-anaheim = Location.create(city: "Anaheim", state: "California", country: "USA")
-disney = anaheim.attractions.create(name: "Disneyland", description: "Happiest place on Earth!")
+
+# anaheim = Location.create(city: "Anaheim", state: "California", country: "USA")
+# disney = anaheim.attractions.create(name: "Disneyland", description: "Happiest place on Earth!")
+disney = Attraction.first
 nadia = User.first
-nadia.trips.create()
+# nadia_disney = nadia.trips.create(start_date: "2019-06-01", end_date: "2019-06-08")
+#
+# nadia_disney.visits.create(attraction: disney, date: "2019-06-01")
+nadia.comments.create(content: "Fun for all", attraction: disney)
