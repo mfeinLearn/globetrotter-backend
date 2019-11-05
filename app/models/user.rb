1c93a88f2f9e0969a306543a33d7cb34b9a38bb4
github username <github email address>
2019-11-05 14:35:40 -0500

    updated user controller show with serializer and adjusted relationships for attraction and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
-
+  has_many :trips
   belongs_to :hometown, class_name: "Location"
+  has_many :trips
+  has_many :visits, through: :trips
+  has_many :attractions, through: :trips
+  has_many :locations, through: :trips
+  has_many :comments
+  # these are generating methods for us that
+  ## active record will use to build
+  ## quearies to our database searching for
+  ## things that are associated by their foreign key 
 end
